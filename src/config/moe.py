48fba6e465b28e8eb8cd772c3268e6954f94234a
Aboli <arshiabolghasemi@gmail.com>
2025-12-22 10:20:39 +0330

    fix moe conifg issue

--- src/config/moe.py
+++ src/config/moe.py
@@ -7,15 +7,12 @@ class MoELoRAConfig:
     r: int
     num_experts: int
     top_k: int
-
     lora_alpha: int = 32
     lora_dropout: float = 0.05
-
     adapter_init: Literal[
         "random", "pissa", "goat", "milora", "goat_mini", "pissa_milora"
     ] = "random"
     router_init: Literal["random", "orthogonal", "svd"] = "random"
-
     target_modules: list[str] = field(
-        default=["q_proj", "k_proj", "v_proj", "o_proj", "qkv_proj"]
+        default_factory=lambda: ["q_proj", "k_proj", "v_proj", "o_proj", "qkv_proj"]
     )
